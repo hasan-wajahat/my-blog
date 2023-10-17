bbce9911aaefa540c12a8e49293d96850512677f
hasan <hasan_wajahat@hotmail.com>
2023-10-17 20:21:48 +0500

    Adds basic colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Analytics } from '@vercel/analytics/react';
 import { Inter } from 'next/font/google';
+import classNames from "classnames";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={classNames(inter.className, 'bg-gray-50', 'dark:bg-gray-900', 'text-black', 'dark:text-white')}>
         {children}
         <Analytics />
       </body>
