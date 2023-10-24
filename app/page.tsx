7cab942fb7124e8ded7808e00f1648cf28e1b626
hasan <hasan_wajahat@hotmail.com>
2023-10-25 01:31:37 +0500

    Improves hero content

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,16 @@ import heroImage from './images/hero.jpeg'
 export default function Home() {
   return (
     <main className="container p-4 mx-auto">
-      <div>
-        <Image src={heroImage} alt="Hero Image" className="max-w-lg" />
+      <div className="max-[350px]:columns-1 columns-2 max-w-4xl mx-auto">
+        <div>
+          <h1 className="font-serif font-bold text-xl md:text-5xl xl:text-6xl">
+            Sharing many years of frontend experience with practical examples.
+          </h1>
+          <h2 className="font-serif my-4 lg:my-8 text-lg md:text-xl">
+            Come and learn all things frontend with me!
+          </h2>
+        </div>
+        <Image src={heroImage} alt="Hero Image" className="max-[350px]:hidden" />
       </div>
     </main>
   );
