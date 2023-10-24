db1f344b0462ca9dad641c3da7a0f4601af5ed04
hasan <hasan_wajahat@hotmail.com>
2023-10-25 01:37:11 +0500

    Fixes indentation

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,16 +1,15 @@
 import Link from "next/link";
 
-
 const Header = () => {
-    return (
-        <header className="container mx-auto px-4 pt-4">
-            <Link href={"/"}>
-                <span className="font-serif font-bold text-xl text-sky-500">
-                    Frontend With Hasan
-                </span>
-            </Link>
-        </header>
-    );
+  return (
+    <header className="container mx-auto px-4 pt-4">
+      <Link href={"/"}>
+        <span className="font-serif font-bold text-xl text-sky-500">
+          Frontend With Hasan
+        </span>
+      </Link>
+    </header>
+  );
 };
 
 export default Header;
