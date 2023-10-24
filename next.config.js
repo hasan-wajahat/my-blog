b8a39542932d14d871e67ffef31e3fbf73879a56
hasan <hasan_wajahat@hotmail.com>
2023-10-25 02:53:09 +0500

    Adds next mdx for markdown

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = nextConfig;
+const withMDX = require('@next/mdx')()
+
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+};
+
+module.exports = withMDX(nextConfig);
