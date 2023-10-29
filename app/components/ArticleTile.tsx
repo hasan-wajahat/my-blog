3decaea621bc1ea44d262254592d4682e04abfd0
hasan <hasan_wajahat@hotmail.com>
2023-10-29 22:40:08 +0500

    Adds read more to article tile

--- app/components/ArticleTile.tsx
+++ app/components/ArticleTile.tsx
@@ -23,10 +23,13 @@ const ArticleTile: React.FC<ArticleTileProps> = ({
       <h3 className="mb-2 font-serif text-xl font-bold group-hover:text-sky-500">
         {title}
       </h3>
-      <p className="mb-2 font-sans text-sm">
+      <p className="mb-2 text-sm">
         By {author} on <time dateTime={date}>{date}</time>
       </p>
-      <p className="max-w-prose font-sans">{excerpt}</p>
+      <p className="mb-2 max-w-prose">{excerpt}</p>
+      <div>
+        Read More <span className="text-sky-500"> -&gt;</span>
+      </div>
     </Link>
   );
 };
