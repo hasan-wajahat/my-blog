287c4a9da5d19a7a9c02a442dce077b1030dc1ae
hasan <hasan_wajahat@hotmail.com>
2023-11-05 02:36:50 +0500

    Fixes links in articles

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,9 +1,19 @@
 import type { MDXComponents } from 'mdx/types';
 import { Code } from 'bright';
+import { AnchorHTMLAttributes, DetailedHTMLProps } from "react";
+
+type CustomLinkProps = DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>;
+
+const CustomLink = ({ children, href }: CustomLinkProps) => (
+  <a href={href} target="_blank" rel="noopener noreferrer">
+    {children}
+  </a>
+);
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
     pre: Code,
+    a: CustomLink,
   };
 }
