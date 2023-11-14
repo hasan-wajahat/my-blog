0043b9aefb6b1bc0c1cb1877996930883d4783c9
hasan <hasan_wajahat@hotmail.com>
2023-11-15 01:12:52 +0500

    Adds sitemap and robot files

---
+++ app/sitemap.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from 'next';
+import { articleList } from './articles/articleList';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const articlesMaps: MetadataRoute.Sitemap = articleList.map((article) => ({
+    url: `https://frontendwithhasan.com/articles/${article.slug}`,
+    lastModified: new Date(),
+    changeFrequency: 'monthly',
+    priority: 0.8,
+  }));
+
+  return [
+    {
+      url: 'https://frontendwithhasan.com',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    ...articlesMaps,
+  ];
+}
