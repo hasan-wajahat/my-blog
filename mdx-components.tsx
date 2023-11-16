8f456a518b9f70b3215157865fe56ee712101cca
hasan <hasan_wajahat@hotmail.com>
2023-11-17 02:35:48 +0500

    Add scroll-to-anchor functionality

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,6 @@
 import type { MDXComponents } from 'mdx/types';
 import { Code } from 'bright';
-import { AnchorHTMLAttributes, DetailedHTMLProps } from "react";
+import { AnchorHTMLAttributes, DetailedHTMLProps, HTMLAttributes } from "react";
 
 type CustomLinkProps = DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>;
 
@@ -10,10 +10,31 @@ const CustomLink = ({ children, href }: CustomLinkProps) => (
   </a>
 );
 
+const toSlug = (text: string) => text.toLowerCase().replace(/\s+/g, '-').replace(/[^\w-]+/g, '');
+
+type HeadingProps = DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>;
+
+const Heading = ({ level, children }: HeadingProps & { level: number }) => {
+  const slug = toSlug(String(children));
+  const Tag = `h${level}` as keyof JSX.IntrinsicElements;
+
+  return (
+    <Tag id={slug}>
+      <a href={`#${slug}`} className="not-prose">{children}</a>
+    </Tag>
+  );
+};
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
     pre: Code,
     a: CustomLink,
+    h1: (props: HeadingProps) => <Heading level={1} {...props} />,
+    h2: (props: HeadingProps) => <Heading level={2} {...props} />,
+    h3: (props: HeadingProps) => <Heading level={3} {...props} />,
+    h4: (props: HeadingProps) => <Heading level={4} {...props} />,
+    h5: (props: HeadingProps) => <Heading level={5} {...props} />,
+    h6: (props: HeadingProps) => <Heading level={6} {...props} />,
   };
 }
