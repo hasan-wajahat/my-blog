ad73ccf4ccc26cc8031cea19c4e8790cab8c3f35
hasan <hasan_wajahat@hotmail.com>
2023-11-24 01:17:11 +0500

    Adds about me to header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,10 +2,15 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="container sticky top-0 mx-auto mb-4 px-4 pt-4 bg-gray-50 dark:bg-gray-900">
+    <header className="container sticky top-0 mx-auto p-4 bg-gray-50 dark:bg-gray-900">
       <Link href={"/"}>
-        <span className="font-serif font-bold text-xl text-sky-500">
+        <span className="font-serif font-bold text-xl text-sky-500 mr-8">
           Frontend With Hasan
+        </span>
+      </Link>
+      <Link href={"/about-me"}>
+        <span className="font-bold text-lg">
+          About Me
         </span>
       </Link>
     </header>
