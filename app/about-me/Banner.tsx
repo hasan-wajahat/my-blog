611874ac59720daf35fe143d5acf65da2733c9cc
hasan <hasan_wajahat@hotmail.com>
2023-11-29 00:44:34 +0500

    Improves main image

--- app/about-me/Banner.tsx
+++ app/about-me/Banner.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import profileImg from '@/images/hasan.webp';
+import profileImg from '@/images/hasan.png';
 
 const Banner = () => {
   const currentYear = new Date().getFullYear();
@@ -9,14 +9,14 @@ const Banner = () => {
       <div className="flex flex-grow flex-shrink basis-0 justify-center">
         <div>
           <p className="my-4">Hello</p>
-          <h2 className="text-5xl mb-6">I&rsquo;m Hasan</h2>
-          <h3 className="text-xl mb-4">A passionate web developer.</h3>
-          <h3 className="text-xl mb-4">With over {experience} years of experience</h3>
-          <h3 className="text-xl">developing web apps.</h3>
+          <h1 className="text-6xl mb-6">I&rsquo;m Hasan</h1>
+          <h2 className="text-2xl mb-4">A passionate web developer.</h2>
+          <h2 className="text-2xl mb-4">With over {experience} years of experience</h2>
+          <h2 className="text-2xl">developing web apps.</h2>
         </div>
       </div>
-      <div className="flex flex-grow flex-shrink basis-0 justify-center bg-gradient-to-r from-sky-500 to-sky-300">
-        <Image src={profileImg} alt="Hasan" className="w-[420px]" />
+      <div className="flex flex-grow flex-shrink basis-0 justify-center bg-gradient-to-r from-transparent to-[#03a8f430] rounded-md">
+        <Image src={profileImg} alt="Hasan" priority className="w-[420px]" />
       </div>
     </div>
   );
--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import Banner from "./Banner";
+
+export const metadata: Metadata = {
+  title: 'Hasan Wajahat CV',
+  description: 'Hasan Wajahat CV/Resume - Frontend Developer',
+};
 
 export default function AboutMe() {
   return (
