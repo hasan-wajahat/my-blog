9f4210718cc70c4bf2f18eef91518f0c75164709
hasan <hasan_wajahat@hotmail.com>
2024-02-25 15:31:23 +0500

    Adds error boundary

---
+++ app/error.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+const ErrorPage = ({ reset }: { reset: () => void }) => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-red-500">Error</h1>
+      <p className="text-lg text-gray-700">Oops! Something went wrong.</p>
+      <div className="mt-4 flex gap-4">
+        <button
+          className="rounded bg-red-500 px-4 py-2 text-white hover:bg-red-600"
+          onClick={() => (window.location.href = '/')}
+        >
+          Go to Home page
+        </button>
+        <button
+          className="rounded bg-red-500 px-4 py-2 text-white hover:bg-red-600"
+          onClick={() => reset()}
+        >
+          Try again
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
